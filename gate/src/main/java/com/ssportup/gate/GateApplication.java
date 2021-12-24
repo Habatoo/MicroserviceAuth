70fa64f53f4313c6284eb6dc99d87ff47c1832d3
habatoo <habatoo@gmail.com>
2021-12-24 22:57:12 +0700

    add zipkin in docker add zuul config - need fix

--- gate/src/main/java/com/ssportup/gate/GateApplication.java
+++ gate/src/main/java/com/ssportup/gate/GateApplication.java
@@ -13,7 +13,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
 public class GateApplication {
     public static void main(String[] args) {
         SpringApplication.run(GateApplication.class, args);
